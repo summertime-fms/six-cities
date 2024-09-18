53a400c5f3a698dd0e778c85fa47ad7cfd34b7c6
summertime-fms <danyushin0107@gmail.com>
2024-09-18 12:36:28 +0300

    Добавляет commandParser для разбора пользовательского ввода

---
+++ src/cli/command-parser.ts
@@ -0,0 +1,18 @@
+type ParsedCommand = Record<string, string[]>
+
+class CommandParser {
+  static parse(args: string[]): ParsedCommand {
+    const parsed: ParsedCommand = {}
+    let currentCommand = '';
+    args.forEach(arg => {
+      if (arg.startsWith('--')) {
+        parsed[arg] = [];
+        currentCommand = arg
+      } else if (currentCommand && arg) {
+        parsed[currentCommand].push(arg)
+      }
+    })
+
+    return parsed;
+  }
+}
